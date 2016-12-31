3eb9e870d22f992594863c3abc4cdf4680ea92dc
Trevor Giddings <tgiddings000@gmail.com>
2016-12-31 15:30:34 -0800

    add stub for search from MainActivity

--- app/src/main/java/com/robocubs4205/cubscout/MainActivity.java
+++ app/src/main/java/com/robocubs4205/cubscout/MainActivity.java
@@ -1,12 +1,17 @@
 package com.robocubs4205.cubscout;
 
+import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.support.v7.widget.SearchView;
 
-import com.robocubs4205.cubscout.admin.GameManagerFragment;
-import com.robocubs4205.cubscout.admin.ScorecardDesignActivity;
+import com.robocubs4205.cubscout.admin.AdminActivity;
 
 /**
  * Created by trevor on 12/28/16.
@@ -14,34 +19,41 @@ import com.robocubs4205.cubscout.admin.ScorecardDesignActivity;
 
 public class MainActivity extends AppCompatActivity {
     MainFragment mainFragment;
-    GameManagerFragment gameManagerFragment;
     final String mainFragmentTag = "mainFragment";
-    final String gameManagerFragmentTag = "gameManagerFragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setSupportActionBar((android.support.v7.widget.Toolbar) findViewById(R.id.toolbar));
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
         mainFragment = (MainFragment) getFragmentManager().findFragmentByTag(mainFragmentTag);
         if (mainFragment == null) {
             mainFragment = new MainFragment();
             getFragmentManager().beginTransaction()
                                 .add(R.id.fragment_container, mainFragment, mainFragmentTag)
                                 .commit();
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent){
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
 
         }
-        gameManagerFragment = (GameManagerFragment) getFragmentManager()
-                .findFragmentByTag(gameManagerFragmentTag);
-        if (gameManagerFragment == null) {
-            gameManagerFragment = new GameManagerFragment();
-            getFragmentManager().beginTransaction()
-                                .add(R.id.fragment_container, gameManagerFragment,
-                                     gameManagerFragmentTag)
-                                .hide(gameManagerFragment)
-                                .commit();
 
-        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        searchItem.setTitle("search games");
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setQueryHint("search games");
+        return super.onCreateOptionsMenu(menu);
     }
 
     @SuppressWarnings("UnusedParameters")
@@ -63,19 +75,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @SuppressWarnings("UnusedParameters")
-    public void LaunchGameDesignActivity(View view) {
-        Intent intent = new Intent(this, ScorecardDesignActivity.class);
+    public void LaunchAdminActivity(View view) {
+        Intent intent = new Intent(this, AdminActivity.class);
         startActivity(intent);
-    }
-
-    @SuppressWarnings("UnusedParameters")
-    public void LaunchGameManager(View view) {
-        getFragmentManager().beginTransaction()
-                            .setCustomAnimations(android.R.animator.fade_in,
-                                                 android.R.animator.fade_out,
-                                                 android.R.animator.fade_in,
-                                                 android.R.animator.fade_out).hide(mainFragment)
-                            .show(gameManagerFragment).addToBackStack(null)
-                            .commit();
     }
 }
